195dc4508e7f87eceaca411ce6ccadb8539b248c
jcbsss <kuba.spala@op.pl>
2024-04-02 08:17:05 +0200

    Euler scheme for one particle. Defined parameters and variables

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ This is a munerical simulation project done for the Geophysical Laboratory I in 
 It is done accordingly to the paper: "The motion of small spherical particles in a cellular flow field" M. R. Maxey
 
 The aim is to conduct a simulation of aerosol parcticles moving in a steady circular flow field. There will be gathered some statistics concerning particles' movement. 
-Additional task is to optimize the code and use paralelization.
+Additional task is to optimize the code and use paralelization. I'm still learning Git
--- main.c
+++ main.c
@@ -1,24 +1,46 @@
-/*Compile with: gcc -o ParticleSim main.c additionalFunctions.c*/
+/*Compile with: gcc -o ParticleSim main.c additionalFunctions.c; ./ParticleSim */
 //#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include "additionalFunctions.h"
+#define PI  3.14159265
 
 main() {
+  /*Simulation Parameters*/
     double end_time = 1.5; //Choose the duration
     double dt = 0.05; //Choose the timestep
-    int time_steps = end_time/dt; // Calculate the number of time steps
-    
+    int time_steps = end_time/dt; // The number of time steps
 
-double* x = allocateDoubleArray(113);
+  /*Flow parameters*/
+    double A=5; //Inertia parameter
+    double W=0.5; //Settling velocity parameter
+    double U=2; //[m/s] //Max flow speed
+    double R=0.3; //Mass ration
+    double L1=1*PI, L2=2*PI; //[m] //Domain size
 
-    for (int i = 1; i < time_steps; ++i)
-  {
-    printf("%d ", i);
-  }
-  
-    printf("\nYou're welcome to this new project %lf %lf %i \n", end_time, dt, time_steps);
-    free(x);
+  /*Defining particle's coordinates and movement*/
+    double* y1 = allocateDoubleArray(time_steps); //Particle's coordinates allocation
+    double* y2 = allocateDoubleArray(time_steps);
+    *y1 = 1; //Defining of the particle's initial coordinates //Need some cool algorithm to place it randomly or uniformly
+    *y2 = 1;
+    double v1=0.3, v2=0.6, a1=0.1, a2=0; //Defining of the particle's initial speeds and accelerations //usually 0
+
+  /*Euler scheme solving the differential equation*/ //There is a chance to place it inside a function
+    for (int i = 0; i < time_steps; ++i)
+    {
+      *(y1+i+1) = *(y1+i) + v1*dt;
+      *(y2+i+1) = *(y2+i) + v2*dt;
+
+      v1 = v1 + a1*dt;
+      v2 = v2 + a2*dt;
+
+      a1 = 1.01*a1+0.01; //You need to upload the real formula here
+      a2 = A*(U-v2 + W); //real formula, this is incomplete. You need some maths, I guess
+
+      //printf("y1 = %lf y2 = %lf  v1 = %lf v2 = %lf   a1 = %lf a2 = %lf \n", *(y1 + i), *(y2 + i), v1, v2, a1, a2); //Optional printing
+
+    free(y1);
+    free(y2);
     return 0;
 }
