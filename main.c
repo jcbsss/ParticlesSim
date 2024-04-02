5a8ecf18538ae3e2483500841cc2d0b7a13c5537
jcbsss <kuba.spala@op.pl>
2024-04-02 08:24:38 +0200

    Simple saving positions to a csv file

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include "additionalFunctions.h"
-#define PI  3.14159265
+#define PI 3.14159265
 
 main() {
   /*Simulation Parameters*/
@@ -37,8 +37,31 @@ main() {
 
       a1 = 1.01*a1+0.01; //You need to upload the real formula here
       a2 = A*(U-v2 + W); //real formula, this is incomplete. You need some maths, I guess
+    }
+
 
       //printf("y1 = %lf y2 = %lf  v1 = %lf v2 = %lf   a1 = %lf a2 = %lf \n", *(y1 + i), *(y2 + i), v1, v2, a1, a2); //Optional printing
+      /*You can put it into some function. CHECK IT*/
+      // Open the file for writing
+    FILE *file = fopen("positions_sParticleSim.csv", "w");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    // Write the header row
+    fprintf(file, "time[s],y1[m],y2[m]\n");
+
+    // Write the data rows
+    for (int i = 0; i < time_steps; i++) {
+        fprintf(file, "%.3f,%.6f,%.6f\n", i*dt, *(y1+i), *(y1+i));
+    }
+
+    // Close the file
+    fclose(file);
+
+    printf("Data written to positions_ParticleSim.csv\n");
+    
 
     free(y1);
     free(y2);
