147c36e9bca5ef3627dd811e03ba466ee8931f8e
jcbsss <kuba.spala@op.pl>
2024-06-01 20:28:54 +0200

    repaired duration clocking

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-// #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,39 +6,47 @@
 #include "additionalFunctions.h"
 #define PI 3.14159265
 
+inline void periodic_boundary(double* y, double L) { //inline keyword gives visible increase of efficiency!
+    if (*y > L) {
+        *y -= L;
+    } else if (*y < 0) {
+        *y += L;
+    }
+}
+
 int main() {
     /* Variables to store the start and end times */
-    clock_t start, end;
+    double start, end;
     double cpu_time_used;
 
     /* Start time */
-    start = clock();
+    start = omp_get_wtime();
 
     /* Flow parameters */
-    double A = 5; // Inertia parameter
-    double W = 0.5; // Settling velocity parameter
-    double U_0 = 1.5; // [m/s] Max flow speed
-    double R = 0.95; // Mass ratio
-    double L1 = 2 * PI, L2 = 4 * PI; // [m] Domain size
+    const double A = 5; // Inertia parameter
+    const double W = 0.5; // Settling velocity parameter
+    const double U_0 = 1.5; // [m/s] Max flow speed
+    const double R = 0.95; // Mass ratio
+    const double L1 = 2 * PI, L2 = 4 * PI; // [m] Domain size
 
     /* Simulation Parameters */
-    double end_time = 100; // Choose the duration
-    double dt = 0.05; // Choose the timestep
-    int time_steps = end_time / dt; // The number of time steps
-    int N = 10000 * 10000; // The number of particles
+    const double end_time = 100; // Choose the duration
+    const double dt = 0.05; // Choose the timestep
+    const int time_steps = end_time / dt; // The number of time steps
+    const int N = 1000 * 1000; // The number of particles
 
     /* Defining particles' initial coordinates */
-    double* y1_current = allocateDoubleArray(N); // Particles' current coordinates
-    double* y2_current = allocateDoubleArray(N);
-    double* y1_next = allocateDoubleArray(N); // Particles' next coordinates
-    double* y2_next = allocateDoubleArray(N);
-    initialize_positions_single(y1_current, y2_current, N, 10000, L1, L2); // Initialize current positions
+    double* y1_current = (double*)aligned_alloc(64, N * sizeof(double));
+    double* y2_current = (double*)aligned_alloc(64, N * sizeof(double));
+    double* y1_next = (double*)aligned_alloc(64, N * sizeof(double));
+    double* y2_next = (double*)aligned_alloc(64, N * sizeof(double));
+    initialize_positions_single(y1_current, y2_current, N, 100, L1, L2); // Initialize current positions
 
     /* Defining particles' initial velocities and accelerations */
-    double* v1 = allocateDoubleArray(N); // Defining the particles' initial speeds and accelerations 
-    double* v2 = allocateDoubleArray(N); // calloc function already initialized 0 values everywhere
-    double* a1 = allocateDoubleArray(N);
-    double* a2 = allocateDoubleArray(N);
+    double* v1 = (double*)aligned_alloc(64, N * sizeof(double)); // Defining the particles' initial speeds and accelerations 
+    double* v2 = (double*)aligned_alloc(64, N * sizeof(double)); // calloc function already initialized 0 values everywhere
+    double* a1 = (double*)aligned_alloc(64, N * sizeof(double));
+    double* a2 = (double*)aligned_alloc(64, N * sizeof(double));
 
     double total_v2_sum = 0.0; // To accumulate the sum of v2 for average calculation
 
@@ -52,15 +59,13 @@ int main() {
             y1_next[j] = y1_current[j] + v1[j] * dt;
             y2_next[j] = y2_current[j] + v2[j] * dt;
 
-            v1[j] = v1[j] + a1[j] * dt;
-            v2[j] = v2[j] + a2[j] * dt;
+            v1[j] += a1[j] * dt;
+            v2[j] += a2[j] * dt;
 
             calculate_accelerations(2, &a1[j], &a2[j], v1[j], v2[j], y1_next[j], y2_next[j], U_0, A, R, W);
 
-            if (y1_next[j] > L1) y1_next[j] -= L1; // Periodic boundary conditions
-            if (y1_next[j] < 0) y1_next[j] += L1;
-            if (y2_next[j] > L2) y2_next[j] -= L2;
-            if (y2_next[j] < 0) y2_next[j] += L2;
+            periodic_boundary(&y1_next[j], L1);
+            periodic_boundary(&y2_next[j], L2);
 
             total_v2_sum += v2[j]; // Accumulate the sum of v2
         }
@@ -74,16 +79,16 @@ int main() {
         y2_next = temp_y2;
     }
 
-    double average_v2 = total_v2_sum / (N * time_steps); // Calculate the overall average velocity v2
+    double average_v2 = total_v2_sum / ((long long)N * (long long)time_steps); // Calculate the overall average settling velocity
 
     printf("\n.....Calculation complete..... \nParameters used: A=%.2lf, W=%.2lf, U_0=%.1lf \nTime=%.2lf, dT=%.5lf, n.o.TimeSteps=%d\n", A, W, U_0, end_time, dt, time_steps);
     printf("Overall average velocity v2: %f\n", average_v2);
 
     /* End time */
-    end = clock();
+    end = omp_get_wtime();
 
     /* Calculate the time used */
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    cpu_time_used = end - start;
     printf("Execution time: %f s \n", cpu_time_used);
 
     /* Clear the space */
